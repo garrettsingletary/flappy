582f0cd3dd164d13fc64f8f0dd64bc082228203a
Garrett Singletary <singletaryge@gmail.com>
2022-02-08 20:14:07 -0800

    adding bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
 }
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .run()
+}
+
