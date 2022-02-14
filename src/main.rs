8305b5be77ff731c7d4e2c47b19d8cd4a18b5a32
Garrett Singletary <singletaryge@gmail.com>
2022-02-13 20:27:43 -0800

    no hold flaps and game over support

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 use bevy::prelude::*;
 
+const WINDOW_HEIGHT: f32 = 500.;
+const WINDOW_WIDTH: f32 = 500.;
 const PLAYER_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 
-#[derive(Bundle)]
+struct GameOverEvent;
+
+#[derive(Component)]
 struct Player {
-    position: Position,
-    velocity: Velocity,
-}
-
-#[derive(Component)]
-struct Velocity {
-    velocity: f32,
-}
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
+    velocity: f32, 
+    space_pressed: bool,
 }
 
 fn setup_camera(mut commands: Commands) {
@@ -36,27 +29,44 @@ fn spawn_player(mut commands: Commands) {
             },
             ..Default::default()
         })
-        .insert_bundle(Player {
-            position: Position { x: 5, y: 5 },
-            velocity: Velocity { velocity: 0.0 },
+        .insert(Player {
+            velocity: 0.0,
+            space_pressed: false,
         });
 }
 
-fn gravity_and_move(mut query: Query<(&mut Velocity, &mut Transform)>) {
-    for (mut velocity, mut transform) in query.iter_mut() {
-        if velocity.velocity < 5.0 {
-            velocity.velocity += 0.2
+fn gravity_and_move(mut game_over_writer: EventWriter<GameOverEvent>, mut query: Query<(&mut Player, &mut Transform)>) {
+    for (mut player, mut transform) in query.iter_mut() {
+        if player.velocity < 5.0 {
+            player.velocity += 0.2
         }
 
-        transform.translation.y -= velocity.velocity;
+        let new_pos: f32 = transform.translation.y - player.velocity;
+
+        if new_pos > -WINDOW_WIDTH && new_pos < WINDOW_HEIGHT {
+            transform.translation.y -= player.velocity;
+        } else {
+            game_over_writer.send(GameOverEvent);
+        }
     }
 }
 
-fn flap(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Velocity>) {
-    for mut velocity in query.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Space) {
-            velocity.velocity -= 1.0;
+fn flap(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Player>) {
+    for mut player in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Space) && player.space_pressed == false {
+            if player.velocity > -10.0 {
+                player.velocity -= 5.0;
+            }
+            player.space_pressed = true;
+        } else if !keyboard_input.pressed(KeyCode::Space) && player.space_pressed {
+            player.space_pressed = false;
         }
+    }
+}
+
+fn game_over(commands: Commands, mut game_over_reader: EventReader<GameOverEvent>) {
+    if game_over_reader.iter().next().is_some() {
+        println!("GAME OVER");
     }
 }
 
@@ -66,6 +76,8 @@ fn main() {
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_player)
         .add_system(gravity_and_move)
-        .add_system(flap.system())
+        .add_system(flap)
+        .add_system(game_over)
+        .add_event::<GameOverEvent>()
         .run()
 }
